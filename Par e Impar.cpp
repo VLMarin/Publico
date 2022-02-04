96c63f54243aecfed9a8bcfba940592d5447e386
LuCamy08 <camy250800@gmail.com>
2022-02-03 18:08:41 -0600

    Update Par e Impar.cpp

--- Par e Impar.cpp
+++ Par e Impar.cpp
@@ -3,6 +3,9 @@
 int main()
 {
  // Zona de Declaración de Variables del Programa principal
+ 
+ printf("BIENVENIDO A LA ZONA DE DECLARACIÓN DE VARIABLES");
+ 
  int numero;
  printf ("Escribe un numero: \n");
  scanf ("%d",&numero);
@@ -12,3 +15,5 @@ int main()
  printf ("El numero es impar\n");
  return 0; // Valor de retorno al S.O.
 } 
+
+//editado por Gpe del Carmen 
